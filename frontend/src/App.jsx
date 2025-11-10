b32abe1cac3f2a849819af16f3a23c0e7d08b9f5
Jeff Murithi <jeffmurithi3@gmail.com>
2025-11-10 20:33:34 +0300

    Prepare frontend for deployment

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-blue-200">
+      <h1 className="text-4xl font-bold text-center text-gray-800">
+        Tailwind is working!
+      </h1>
+    </div>
+  )
+}
+
+export default App;
